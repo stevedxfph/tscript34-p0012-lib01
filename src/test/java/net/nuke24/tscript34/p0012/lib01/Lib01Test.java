32660d4c2ac3929ec0b0d0a5079b2469255a1a01
TOGoS <togos00@gmail.com>
2023-10-12 14:08:45 -0500

    Add a unit test

---
+++ src/test/java/net/nuke24/tscript34/p0012/lib01/Lib01Test.java
@@ -0,0 +1,9 @@
+package net.nuke24.tscript34.p0012.lib01;
+
+import junit.framework.TestCase;
+
+public class Lib01Test extends TestCase {
+	public void testMe() {
+		assertFalse("j00 win it!", false);
+	}
+}
