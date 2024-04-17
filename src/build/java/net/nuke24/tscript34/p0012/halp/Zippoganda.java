ad01233bca046be3f8d113b83cdfe681bce5e8a5
TOGoS <togos00@gmail.com>
2024-04-17 14:00:02 -0500

    Almost generate SHA1 and MD5 files

--- src/build/java/net/nuke24/tscript34/p0012/halp/Zippoganda.java
+++ src/build/java/net/nuke24/tscript34/p0012/halp/Zippoganda.java
@@ -1,19 +1,17 @@
 package net.nuke24.tscript34.p0012.halp;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.URLDecoder;
 import java.nio.charset.Charset;
-import java.nio.file.attribute.FileTime;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.zip.ZipEntry;
@@ -110,6 +108,20 @@ class ZipBlob implements Blob {
 public class Zippoganda {
 	static final Charset UTF8 = Charset.forName("UTF-8");
 	
+	static char hexDigit(int value) {
+		if( value < 10 ) return (char)('0'+value);
+		return (char)('a' - 10 + value);
+	}
+	
+	static String hexEncode(byte[] data) {
+		char[] hex = new char[data.length*2];
+		for( int i=0; i<data.length; ++i ) {
+			hex[i*2+0] = hexDigit(0xF & (data[i] >> 4));
+			hex[i*2+1] = hexDigit(0xF & (data[i] >> 0));
+		}
+		return new String(hex);
+	}
+	
 	static class FileBlobResolver implements Resolver<Blob> {
 		final private File root;
 		public FileBlobResolver(File root) {
@@ -161,11 +173,91 @@ public class Zippoganda {
 		}
 	}
 	
+	interface Action {
+		// Not necjknt the best design idk
+		public int run(OutputStream out) throws IOException;
+	}
+	
+	static class HashifyAction implements Action {
+		final List<String> roots;
+		public HashifyAction(List<String> roots) {
+			this.roots = roots;
+		}
+		
+		public void hashify(String name, File f, OutputStream out) throws IOException {
+			if( f.isDirectory() ) {
+				for( File s : f.listFiles() ) {
+					hashify(name+"/"+s.getName(), s, out);
+				}
+			} else {
+				MessageDigest sha1;
+				MessageDigest md5;
+				try {
+					sha1 = MessageDigest.getInstance("SHA1");
+					md5 = MessageDigest.getInstance("MD5");
+				} catch (NoSuchAlgorithmException e) {
+					throw new RuntimeException(e);
+				}
+				FileInputStream fis = new FileInputStream(f);
+				try {
+					byte[] buffer = new byte[65536];
+					int z;
+					while( (z = fis.read(buffer)) > 0 ) {
+						sha1.update(buffer, 0, z);
+						md5.update(buffer, 0, z);
+					}
+					out.write((name+".sha1\tdata:,"+hexEncode(sha1.digest())+"\n").getBytes(UTF8));
+					out.write((name+".md5\tdata:,"+hexEncode(md5.digest())+"\n").getBytes(UTF8));
+				} finally {
+					fis.close();
+				}
+			}
+		}
+		
+		@Override public int run(OutputStream out) throws IOException {
+			for( String root : roots ) {
+				hashify(root, new File(root), out);
+			}
+			return 0;
+		}
+		
+		public static HashifyAction parse(String[] argv, int i) {
+			List<String> roots = new ArrayList<>();
+			for( ; i<argv.length; ++i ) {
+				if( argv[i].startsWith("-") ) {
+					throw new RuntimeException("hashify: Unrecognized argument: "+argv[i]);
+				} else {
+					roots.add(argv[i]);
+				}
+			}
+			return new HashifyAction(roots);
+		}
+	}
+	
 	public static void main(String[] args) throws IOException {
 		Resolver<Blob> blobResolver = new MiltiResolver<Blob>(Arrays.asList(
 			new DataUriResolver(),
 			new FileBlobResolver(new File("."))
 		));
+		
+		Action action = null;
+		int i=0;
+		for( ; i<args.length; ++i ) {
+			if( "hashify".equals(args[i]) ) {
+				action = HashifyAction.parse(args, i+1);
+				break;
+			} else {
+				System.err.println("Unrecognized argument: "+args[i]);
+				System.exit(1);
+			}
+		}
+		
+		if( action == null ) {
+			System.err.println("No action specified");
+			System.exit(1);
+		}
+		
+		System.exit(action.run(System.out));
 		
 		// Okay so we gotta get dumb old Maven to generate three JAR files:
 		// the regular one, one with sources, and one with 'javadoc',
@@ -183,19 +275,5 @@ public class Zippoganda {
 		// inside a zip file, and then you can go to some web page,
 		// maybe near https://central.sonatype.com/publishing, and upload
 		// the zip file, and maybe it will accept it.
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String line;
-		while( (line = br.readLine()) != null ) {
-			line = line.trim();
-			if( line.startsWith("#") || line.isEmpty() ) continue;
-			
-			String[] tokens = line.split("\\s+");
-			for( String t : tokens ) {
-				Blob b = blobResolver.get(t);
-				if( b == null ) System.err.println("Failed to resolve "+t);
-				else b.writeTo(System.out);
-			}
-		}
 	}
 }
